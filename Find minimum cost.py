8cbe86dd167967bfd21e840aec5abf53b2df0853
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-27 19:35:57 +0530

    Add files via upload

---
+++ Find minimum cost.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def minCost(self, n : int, a : List[int]) -> int:
+        # code here
+        cost_to_even = 0
+
+        cost_to_odd = 0
+
+        for x in a:
+            if x % 2 == 0:
+                cost_to_odd += x
+            else:
+                cost_to_even += x
+        return min(cost_to_even, cost_to_odd)
