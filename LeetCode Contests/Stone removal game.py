5eee6f20d7f0b95ca679678673d383b6f935cbf0
prathammkc <chelaramanipratham@gmail.com>
2024-11-24 08:34:51 +0530

    DOne

--- LeetCode Contests/Stone removal game.py
+++ LeetCode Contests/Stone removal game.py
@@ -1,15 +1,15 @@
 class Solution:
     def canAliceWin(self, n: int) -> bool:
         if n < 10:
-            return False  
-    
+            return False
+
         stones_to_remove = 10
         is_alice_turn = True  # Alice starts
-        
+
         while n >= stones_to_remove:
             n -= stones_to_remove
             stones_to_remove -= 1
             is_alice_turn = not is_alice_turn  # Alternate turns
-        
+
 
         return not is_alice_turn
