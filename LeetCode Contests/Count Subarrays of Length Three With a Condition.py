74e8ec12f26830b3276cbd63e842320c2e1163d4
thepratholic <chelaramanipratham@gmail.com>
2024-12-21 20:12:44 +0530

    21/12/24

---
+++ LeetCode Contests/Count Subarrays of Length Three With a Condition.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        if n < 3: return 0
+        l, r = 0, 2
+
+        while r < n:
+            if (nums[r] + nums[l]) == nums[r-1] / 2:
+                count += 1
+
+            r += 1
+            l += 1
+
+        return count
