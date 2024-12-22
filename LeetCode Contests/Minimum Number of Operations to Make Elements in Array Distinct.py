9842939e8e589cb52270b0ed795e0e5982912379
thepratholic <chelaramanipratham@gmail.com>
2024-12-22 08:43:32 +0530

    22/12/24

---
+++ LeetCode Contests/Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        if len(nums) == len(set(nums)):
+            return 0
+
+        operations = 0
+        while nums:
+            nums = nums[3:]
+            operations += 1
+
+            if len(nums) == len(set(nums)):
+                return operations
+
+        return operations
