2b2ddbd9c4ed0648fa901574322cd40a39f31766
thepratholic <chelaramanipratham@gmail.com>
2025-02-10 08:38:33 +0530

    Upsolved

---
+++ Codeforces/Codeforces Contests/Contest 1003 Div 4/C_2_Skibidus_and_Fanum_Tax_hard_version.py
@@ -0,0 +1,31 @@
+from bisect import bisect_left
+
+def solve():
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    prev = float('-inf')
+    b.sort()
+
+    for i in range(n):
+        idx = bisect_left(b, a[i] + prev)
+
+        if idx < m:
+            val = b[idx]
+
+            if a[i] < prev:
+                a[i] = val - a[i]
+            else:
+                a[i] = min(a[i], val - a[i])
+
+        if a[i] < prev:
+            print("NO")
+            return
+        prev = a[i]
+
+    print("YES")
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
