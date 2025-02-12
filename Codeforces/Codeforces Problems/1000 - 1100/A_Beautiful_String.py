098c6f4248242e2ffe1fa6bf55aa3b274808b1b0
thepratholic <chelaramanipratham@gmail.com>
2025-02-12 09:01:14 +0530

    Done with changes

--- Codeforces/Codeforces Problems/1000 - 1100/A_Beautiful_String.py
+++ Codeforces/Codeforces Problems/1000 - 1100/A_Beautiful_String.py
@@ -1,8 +1,8 @@
 def solve():
-    s = [c for c in input()]
+    s = list(input())
 
 
-    for i in range(len(s) - 1):
+    for i in range(len(s)):
         if s[i] == '?':
             prev = 'd' if i == 0 else s[i-1]
             nxt = 'e' if i+1 >= len(s) else s[i+1]
@@ -15,7 +15,6 @@ def solve():
     for i in range(len(s) - 1):
         if s[i] == s[i+1]:
             print("-1")
-
             return
 
     print("".join(s))
