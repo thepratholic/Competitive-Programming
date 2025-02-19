d80fa60680bbe24b238090c9a054fc315755ca2d
thepratholic <chelaramanipratham@gmail.com>
2025-02-20 05:13:26 +0530

    20/2/25

--- CodeChef/Starters 174/Bigger_is_Better.py
+++ CodeChef/Starters 174/Bigger_is_Better.py
@@ -1,42 +1,15 @@
-def solve_restaurant_name(N, A):
-    if N == 1 and A == 'z':
-        return "-1"
-        
-    mid = (N-1) // 2
-    result = list(A)
-    
-    for i in range(mid + 1):
-        orig_char = result[i]
-        
-        found = False
-        for c in range(ord('a'), ord('z') + 1):
-            result[i] = chr(c)
-            temp = ''.join(result)
+def solve():
+    n = int(input())
 
-            if temp > A and temp[::-1] > A:
-                found = True
-                break
+    a = input()
 
-        if not found:
-            result[i] = orig_char
-            result[N-1-i] = orig_char
+    ans = ""
 
-    final = ''.join(result)
-    if final > A and final[::-1] > A:
-        return final
+    for i in range(n):
+        ans += "z"
 
-    result = list(A)
-    if result[0] < 'z':
-        result[0] = chr(ord(result[0]) + 1)
-        for i in range(1, N):
-            result[i] = 'a'
-        return ''.join(result)
-    
-    return "-1"
+    print(ans if ans > a else "-1")
 
-# Read input
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    A = input().strip()
-    print(solve_restaurant_name(N, A))
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        solve()
