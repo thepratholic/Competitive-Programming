63f7e2ce4a98c880ac81bedefe66f8c72d0b2953
thepratholic <chelaramanipratham@gmail.com>
2025-02-22 08:21:01 +0530

    22/2/25

---
+++ B_Set_of_Strangers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define endl '\n'
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+void solve() {
+    // Your code here
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
