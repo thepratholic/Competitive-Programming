ca8238a81e39b69dd80c367b69744d04e0b291c5
thepratholic <chelaramanipratham@gmail.com>
2025-02-23 08:39:51 +0530

    23/2/25

---
+++ LeetCode/Weekly contest 438/Check If Digits Are Equal in String After Operations I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        while len(s) > 2:
+            new_s = ""
+            for i in range(len(s) - 1):
+                new_s += str((int(s[i]) + int(s[i+1])) % 10)
+            s = new_s
+
+        return s[0] == s[1]
