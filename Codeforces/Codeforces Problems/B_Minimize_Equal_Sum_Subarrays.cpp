3c4c22ee5c877fbe2dcbec7640be2373b95c9840
thepratholic <chelaramanipratham@gmail.com>
2025-02-23 21:42:51 +0530

    23/2/25

---
+++ Codeforces/Codeforces Problems/B_Minimize_Equal_Sum_Subarrays.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros for faster coding
+#define int long long
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define endl '\n'
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+// Debugging Macros
+#define debug(x) cerr << #x << " = " << (x) << endl;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    for(int i = 1; i < n;i++) {
+        cout << v[i] << " ";
+    }
+    cout << v[0] << endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
