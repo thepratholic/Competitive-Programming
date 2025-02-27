8b1b22c5eebb2275d969d75c49bd29c4af3e72a9
thepratholic <chelaramanipratham@gmail.com>
2025-02-27 13:45:28 +0530

    27/2/25

--- CodeChef/Starters 175/Technex_Tickets.py
+++ CodeChef/Starters 175/Technex_Tickets.py
@@ -1,20 +1,20 @@
 def solve(N):
     queue = list(range(1, N+10))
     seconds = 0
-    
+
     while True:
         seconds += 1
-        
+
         first = queue[0]
         third = queue[2] if len(queue) > 2 else -1
-        
+
         if first == N or third == N:
             return seconds
 
         queue.pop(0)
         if len(queue) > 1:
             queue.pop(1)
-    
+
 T = int(input())
 
 for _ in range(T):
