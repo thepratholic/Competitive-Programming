0a4c0461dd060cfe768eccddf3973b2fde8ecf28
thepratholic <chelaramanipratham@gmail.com>
2025-03-02 16:39:00 +0530

    2/3/25

---
+++ Codeforces/practice problems/B_Queue_at_the_School.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("unroll-loops")
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    int n, t;
+    cin >> n >> t;
+    string s;
+    cin >> s;
+
+    while(t--) {
+        for(int i = 0;i < n - 1;i++) {
+            if(s[i] == 'B' && s[i+1] == 'G') {
+                swap(s[i], s[i+1]);
+                i ++;
+            }
+        }
+    }
+
+    cout << s << endl;
+}
+
+int main()
+{
+#ifdef PRADY
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef PRADY
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
