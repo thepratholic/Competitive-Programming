0b88a6654cd7f8979f7ca0842eb5d0ce3bc7cb29
thepratholic <chelaramanipratham@gmail.com>
2025-03-03 09:24:53 +0530

    3/3/25

---
+++ Codeforces/practice problems/A_Reconnaissance_2.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    int a[n];
+    ll mini = INT_MAX;
+
+    for(int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+
+    ll idx1 = 1, idx2 = 2;
+
+    for(int i = 0;i < n;i++) {
+        int next = (i+1) % n;
+        int diff = abs(a[i] - a[next]);
+
+        if(diff < mini) {
+            mini = diff;
+            idx1 = i+1;
+            idx2 = next + 1;
+        }
+    }
+
+    cout << idx1 << " " << idx2 << endl;
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
