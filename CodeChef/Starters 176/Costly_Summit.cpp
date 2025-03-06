6b9a6e8bbffad80d92a9ab667047da0edb3119e4
thepratholic <chelaramanipratham@gmail.com>
2025-03-06 10:44:23 +0530

    6/3/25

---
+++ CodeChef/Starters 176/Costly_Summit.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll n, c;
+    cin >> n >> c;
+    string s;
+    cin >> s;
+    ll ans = 5 * c;
+    for (ll i = 0; i < 32; i++)
+    {
+        ll translator = 1;
+        ll cost = 0;
+        for (ll j = 0; j < 5; j++)
+        {
+            if (i & (1ll << j))
+            {
+                cost += c;
+            }
+        }
+        for (ll j = 0; j < n; j++)
+        {
+            if (1ll << (s[j] - 'A') & i)
+            {
+            }
+            else
+            {
+                cost += translator++;
+            }
+        }
+        ans = min(cost, ans);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
