c7ce0ed1bb3901accb1e6ce0b8d4f854d4808b1f
thepratholic <chelaramanipratham@gmail.com>
2025-03-19 19:23:08 +0530

    19/3/25

---
+++ Codeforces/practice problems/B_K_th_Common_Divisor.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll a, b, k;
+    cin >> a >> b >> k;
+
+    ll num = __gcd(a, b);
+
+    vector<ll> v;
+
+    for(ll i = 1; i * i <= num; i++) {
+        if(num % i == 0) {
+            v.push_back(i);
+
+            if(i != (num / i)) {
+                v.push_back(num / i);
+            }
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << v[v.size() - k] << endl;
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
