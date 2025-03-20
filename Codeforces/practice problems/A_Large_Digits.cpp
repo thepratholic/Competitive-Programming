341dee0c146b8667acabbcdd19144082437f0f2f
thepratholic <chelaramanipratham@gmail.com>
2025-03-20 08:24:11 +0530

    20/3/25

---
+++ Codeforces/practice problems/A_Large_Digits.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+
+    ll sum1 = 0;
+    while(a > 0) {
+        ll ld = a % 10;
+        sum1 += ld;
+        a /= 10;
+    }
+
+    ll sum2 = 0;
+    while(b > 0) {
+        ll ld = b % 10;
+        sum2 += ld;
+        b /= 10;
+    }
+
+    if(sum1 == sum2) {
+        cout << sum1 << endl;
+    }
+
+    else {
+        cout << max(sum1, sum2) << endl;
+    }
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
