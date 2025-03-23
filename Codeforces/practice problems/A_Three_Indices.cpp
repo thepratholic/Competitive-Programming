38e9db662ccfa496a7587faef124791268aadb98
thepratholic <chelaramanipratham@gmail.com>
2025-03-23 21:34:20 +0530

    23/3/25

--- Codeforces/practice problems/A_Three_Indices.cpp
+++ Codeforces/practice problems/A_Three_Indices.cpp
@@ -7,39 +7,19 @@ using namespace std;
 
 void solve() {
     ll n;
-    cin >> n;
+	cin >> n;
 
-    vector<ll> p(n);
-    for (ll &x : p) {
-        cin >> x;
-    }
-
-    bool found = false;
-    ll i_ans = -1, j_ans = -1, k_ans = -1;
-
-    for (ll j = 1; j < n - 1 && !found; j++) {
-        for (ll i = 0; i < j; i++) {
-            if (p[i] < p[j]) {
-                for (ll k = j + 1; k < n; k++) {
-                    if (p[k] < p[j]) {
-                        i_ans = i;
-                        j_ans = j;
-                        k_ans = k;
-                        found = true;
-                        break;
-                    }
-                }
-                if (found) break;
-            }
-        }
-    }
-
-    if (found) {
-        cout << "YES" << nline;
-        cout << i_ans + 1 << " " << j_ans + 1 << " " << k_ans + 1 << nline;
-    } else {
-        cout << "NO" << nline;
-    }
+    vector<int> a(n);
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+	for (int i = 1; i < n - 1; ++i) {
+		if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+			cout << "YES" << endl;
+			cout << i << ' ' << i + 1 << ' ' << i + 2 << endl;
+			return;
+		}
+	}
+	cout << "NO" << endl;
 }
 
 int main() {
