859d81c1f58120afb2bc7ae737b0c6069464a9c9
thepratholic <chelaramanipratham@gmail.com>
2025-04-08 16:24:27 +0530

    8/4/25

---
+++ Codeforces/practice problems/A_Divide_and_Conquer.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n);
+    ll odd = 0;
+    for(ll &x : v) {
+        cin >> x;
+        if(x % 2 == 1) odd ++;
+
+    }
+
+    ll ans = INT_MAX;
+    if(odd % 2 == 0) {
+        cout << 0 << endl;
+        return;
+    }
+
+    else{ 
+        for(ll i = 0; i < n; i++) {
+            if(v[i] % 2 == 0) {
+                ll cnt = 0;
+                while(v[i] % 2 != 1) {
+                    v[i] /= 2;
+                    cnt++;
+                }
+                ans = min(ans, cnt);
+            }
+
+            else {
+                ll cnt = 0;
+                while(v[i] % 2 != 0) {
+                    v[i] /= 2;
+                    cnt++;
+                }
+                ans = min(ans, cnt);
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
