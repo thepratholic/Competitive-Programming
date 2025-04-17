f486a3020da90878acaced853c91db836c45dd31
thepratholic <chelaramanipratham@gmail.com>
2025-04-17 23:07:17 +0530

    17/4/25

---
+++ Codeforces/practice problems/A_Odd_Selection.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    int o = 0, e = 0;
+    for (int i = 0; i < n; i++) {
+        int v;
+        cin >> v;
+        if (v & 1) o++;
+        else       e++;
+    }
+
+    int take_odd = min(o, x);
+    if (take_odd % 2 == 0) take_odd--; 
+    if (take_odd >= 1 && x - take_odd <= e) 
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
