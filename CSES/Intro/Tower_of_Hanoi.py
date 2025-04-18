c4ef0d6dc97add15e0c174078a6a5344e0fdabb5
thepratholic <chelaramanipratham@gmail.com>
2025-04-18 17:24:43 +0530

    18/4/25

---
+++ CSES/Intro/Tower_of_Hanoi.py
@@ -0,0 +1,19 @@
+def f(n, s, a, d, moves):
+    if n == 1:
+        moves.append((s, d))
+
+    else:
+        f(n-1, s, d, a, moves)
+        moves.append((s, d))
+        f(n-1, a, s, d, moves)
+
+def solve(n):
+    moves = []
+    f(n, 1, 2, 3, moves)
+    print(len(moves))
+    for i, j in moves:
+        print(i, j)
+
+if __name__ == "__main__":
+    n = int(input())
+    solve(n)
