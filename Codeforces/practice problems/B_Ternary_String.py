7fb050b5ca32642a7e0753f3228fbc8d78343ced
thepratholic <chelaramanipratham@gmail.com>
2025-04-20 16:27:35 +0530

    20/4/25

---
+++ Codeforces/practice problems/B_Ternary_String.py
@@ -0,0 +1,68 @@
+import sys, io, os
+import math
+import bisect
+import heapq
+import string
+import re
+from decimal import *
+from random import randrange, choice, shuffle
+from types import GeneratorType
+from collections import defaultdict, Counter, deque
+
+input = sys.stdin.readline
+
+def I(): return input().strip()
+def II(): return int(input())
+def MII(): return map(int, input().split())
+def LI(): return list(input().split())
+def LII(): return list(map(int, input().split()))
+def GMI(): return map(lambda x: int(x) - 1, input().split())
+def LGMI(): return list(map(lambda x: int(x) - 1, input().split()))
+def WRITE(out): return print('\n'.join(map(str, out)))
+def WS(out): return print(' '.join(map(str, out)))
+def WNS(out): return print(''.join(map(str, out)))
+def WSNOPRINT(out): return ''.join(map(str, out))
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+
+#@bootstrap
+def solve():
+    # Your solution here
+    s = I()
+    count = {'1': 0, '2': 0, '3': 0}
+
+    l = 0
+    ans = float("inf")
+    for r in range(len(s)):
+        count[s[r]] += 1
+
+        while count['1'] > 0 and count['2'] > 0 and count['3'] > 0:
+            ans = min(ans, r - l + 1)
+            count[s[l]] -= 1
+            l += 1
+
+    print(0 if ans == float("inf") else ans)
+
+
+def main():
+    for _ in range(int(input())):
+        solve()
+
+main()
