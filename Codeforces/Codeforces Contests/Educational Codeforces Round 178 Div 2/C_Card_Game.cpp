db415ee443af8143e56aaa9838e0515c01c6fc8b
thepratholic <chelaramanipratham@gmail.com>
2025-05-10 09:37:57 +0530

    Upsolved C of Edu Round 178

---
+++ Codeforces/Codeforces Contests/Educational Codeforces Round 178 Div 2/C_Card_Game.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    if(s[0] == s.back()) {
+        if(s[0] == 'A') {
+            cout << "Alice" << nline;
+        }
+
+        else {
+            cout << "Bob" << nline;
+        }
+    }
+
+    else if (s[0] == 'A' && s.back() == 'B') {
+        if(count(begin(s), end(s), 'B') == 1) {
+            cout << "Alice" << nline;
+        }
+
+        else {
+            cout << "Bob" << nline;
+        }
+    }
+
+    else {
+        if(n == 2) {
+            cout << "Bob" << nline;
+        }
+
+        else {
+            if(s[n - 2] == 'A') {
+                cout << "Alice" << nline;
+            }
+
+            else {
+                cout << "Bob" << nline;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
