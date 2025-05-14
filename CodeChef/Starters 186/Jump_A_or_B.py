9379d84579ff48dfcc42b550035a41fb2a68be58
thepratholic <chelaramanipratham@gmail.com>
2025-05-14 20:35:44 +0530

    14/5/25

---
+++ CodeChef/Starters 186/Jump_A_or_B.py
@@ -0,0 +1,82 @@
+import sys, io, os
+import math
+import bisect
+import heapq
+import string
+import re
+from decimal import *
+from random import randrange, choice, shuffle
+from types import GeneratorType
+from collections import defaultdict, Counter, deque
+
+input = sys.stdin.readline
+
+def I(): return input()
+def II(): return int(input())
+def MII(): return map(int, input().split())
+def LI(): return list(input().split())
+def LII(): return list(map(int, input().split()))
+def GMI(): return map(lambda x: int(x) - 1, input().split())
+def LGMI(): return list(map(lambda x: int(x) - 1, input().split()))
+def WRITE(out): return print('\n'.join(map(str, out)))
+def WS(out): return print(' '.join(map(str, out)))
+def WNS(out): return print(''.join(map(str, out)))
+def WSNOPRINT(out): return ''.join(map(str, out))
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+
+#@bootstrap
+def solve():
+    # Your solution here
+    n, m, a, b = MII()
+    
+    if a == b:
+        if a * n == m:
+            print("Yes")
+        else:
+            print("No")
+        return
+
+    num = m - b * n
+    den = a - b
+
+    if den == 0:
+        print("No")
+        return
+
+    if num % den != 0:
+        print("No")
+        return
+
+    x = num // den
+    if 0 <= x <= n:
+        print("Yes")
+    else:
+        print("No")
+
+        
+    
+        
+    
+
+def main():
+    for _ in range(II()):
+        solve()
+main()
