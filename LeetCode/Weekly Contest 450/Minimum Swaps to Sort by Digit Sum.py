5a9f69c475574392471a8efade8e5490ccea05a9
thepratholic <chelaramanipratham@gmail.com>
2025-05-20 16:38:53 +0530

    Upsolved

---
+++ LeetCode/Weekly Contest 450/Minimum Swaps to Sort by Digit Sum.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def getSum(self, n):
+        res = 0
+        while n:
+            res += (n % 10)
+            n //= 10
+        return res
+
+    def minSwaps(self, nums: List[int]) -> int:
+        n = len(nums)
+        digitPair = []
+
+        for i in range(n):
+            sum_of_digit = self.getSum(nums[i])
+            digitPair.append((sum_of_digit, nums[i], i))
+
+        digitPair.sort(key = lambda x : (x[0], x[1]))
+
+        position = [0] * n
+        for sorted_idx in range(n):
+            original_index = digitPair[sorted_idx][2]
+            position[original_index] = sorted_idx
+
+        vis = [False] * n
+        swaps = 0
+        for i in range(n):
+            if vis[i] or position[i] == i:
+                continue
+
+            cycles = 0
+            j = i
+            while not vis[j]:
+                vis[j] = True
+                j = position[j]
+                cycles += 1
+
+            swaps += cycles - 1
+
+        return swaps
