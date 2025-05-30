eb03f6df16f1e37cb8e3b7ce1ff0c7e09d3d2ba7
thepratholic <chelaramanipratham@gmail.com>
2025-05-30 18:24:49 +0530

    30/5/25

---
+++ Codeforces/practice problems/C_Earning_on_Bets.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n) \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+// read question properly
+// don't forget newlines!!!!!!
+// take care about cin >> t;
+// comment the optimization before debugging
+// ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+    return a;
+}
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    iv(k, n);
+    
+    int z = 1;
+    for (int i = 0; i < n; i++) {
+        z = lcm(z, k[i]);
+    }
+    
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        suma += z / k[i];
+    }
+    
+    if (suma < z) {
+        for (int i = 0; i < n; i++) {
+            cout << z / k[i] << " ";
+        }
+        cout << "\n";
+    } else {
+        cout << -1 << "\n";
+    }
+}
+
+int main()
+{
+#ifdef thepratholic
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    clock_t T = clock();
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+#ifdef thepratholic
+    cout << "\nTime taken: " << ((float)(clock() - T)) / CLOCKS_PER_SEC << " seconds";
+#endif
+    return 0;
+}
