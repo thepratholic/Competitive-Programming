da9cca1bc15bd355053d41413c171bcbe2c5063a
thepratholic <chelaramanipratham@gmail.com>
2025-06-02 15:06:41 +0530

    Upsolved

--- Codeforces/Codeforces Contests/Contest 1026 Div 2/C_Racing.cpp
+++ Codeforces/Codeforces Contests/Contest 1026 Div 2/C_Racing.cpp
@@ -23,15 +23,48 @@ using namespace std;
 // comment the optimization before debugging
 // ALWAYS USE FIXED << SETPRECISION WHILE OUTPUTTING FLOATS
 
-void solve()
-{
-    ll n;
+void solve() {
+    int n;
     cin >> n;
-
-    iv(p, n);
-    iv(q, n);
-
-        
+    vector<int> d(n);
+    for (auto &x : d) {
+        cin >> x;
+    }
+    vector<int> l(n), r(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> l[i] >> r[i];
+    }
+    int left = 0;
+    vector<int> last;
+    for (int i = 0; i < n; ++i) {
+        if (d[i] == -1) {
+            last.push_back(i);
+        } else {
+            left += d[i];
+        }
+        while (left < l[i]) {
+            if (last.empty()) {
+                cout << -1 << nline;
+                return;
+            }
+            d[last.back()] = 1;
+            ++left;
+            last.pop_back();
+        }
+        while (left + last.size() > r[i]) {
+            if (last.empty()) {
+                cout << -1 << nline;
+                return;
+            }
+            d[last.back()] = 0;
+            last.pop_back();
+        }
+    }
+    for (auto &x : d) {
+        cout << max(0, x) << " ";
+    }
+    cout << nline;
+    return;
 }
 
 int main()
