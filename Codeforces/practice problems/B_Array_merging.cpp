f9eabb3ad92c1114e28b90431f3c297612c43506
thepratholic <chelaramanipratham@gmail.com>
2025-08-12 21:42:08 +0530

    CF Problm

---
+++ Codeforces/practice problems/B_Array_merging.cpp
@@ -0,0 +1,81 @@
+// Choose cpp when the time
+// limit will for sure give tle in pypy
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define INF LLONG_MAX
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define print(arr) for (auto it : arr){cout<<it<<" ";}cout<<endl;
+#define len(arr) arr.size()
+#define printf(x) cout << x << endl;
+#define printm(map) cout<<"{";for(auto it: map){cout<<it.first<<":"<<it.second<<",";};cout<<"}"<<endl;
+
+// const int MOD = 998244353;
+const int MOD = 1e9 + 7;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+// String hashing: sh/shclass, Number: numtheory, SparseTable: SparseTable
+// Segment Tree(lazy propogation): SegmentTree, Merge Sort Tree: sorttree
+// binary indexed tree: BIT, Segment Tree(point updates): SegmentPoint, Convex Hull: hull, Trie/Treap: Tries
+// Combinatorics: pnc, Diophantine Equations: dpheq, Graphs: graphs, DSU: DSU, Geometry: Geometry, FFT: fft
+// Persistent Segment Tree: perseg, FreqGraphs: bgraph
+// Template : https://github.com/thepratholic/CP-Template-Py-Cpp
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    unordered_map<int, int> max_a, max_b;
+
+    int cnt = 1;
+    for (int i = 1; i <= n; i++) {
+        if (i < n && a[i] == a[i - 1]) cnt++;
+        else {
+            max_a[a[i - 1]] = max(max_a[a[i - 1]], cnt);
+            cnt = 1;
+        }
+    }
+
+    cnt = 1;
+    for (int i = 1; i <= n; i++) {
+        if (i < n && b[i] == b[i - 1]) cnt++;
+        else {
+            max_b[b[i - 1]] = max(max_b[b[i - 1]], cnt);
+            cnt = 1;
+        }
+    }
+
+    int ans = 0;
+    for (auto &p : max_a) {
+        int val = p.first;
+        ans = max(ans, max_a[val] + max_b[val]);
+    }
+    for (auto &p : max_b) {
+        int val = p.first;
+        ans = max(ans, max_a[val] + max_b[val]);
+    }
+
+    cout << ans << "\n";
+}
+
+int32_t main() {
+    fastio
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
+
+
+
