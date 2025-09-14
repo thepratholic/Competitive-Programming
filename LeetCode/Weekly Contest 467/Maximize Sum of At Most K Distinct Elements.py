8f77ec3338eeef6e09e9ff34448786ba0c87d7a7
thepratholic <chelaramanipratham@gmail.com>
2025-09-14 08:33:38 +0530

    LC weekly 467

---
+++ LeetCode/Weekly Contest 467/Maximize Sum of At Most K Distinct Elements.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+
+        nums = list(set(nums))
+
+        nums.sort(reverse = True)
+
+        return nums[:k]
