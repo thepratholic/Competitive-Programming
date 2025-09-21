a2df848e7d36203bb0cd8092472bc2b53028ace8
thepratholic <chelaramanipratham@gmail.com>
2025-09-21 08:35:43 +0530

    LC weekly 468

---
+++ LeetCode/Weekly Contest 468/Bitwise OR of Even Numbers in an Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def evenNumberBitwiseORs(self, nums: List[int]) -> int:
+        ans = 0
+
+        for num in nums:
+            if num % 2 == 0:
+                ans |= num
+
+        return ans
