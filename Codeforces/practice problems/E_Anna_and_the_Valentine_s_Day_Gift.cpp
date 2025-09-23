722fda8c471ff4788c15e6f90650a4eff8df9406
thepratholic <chelaramanipratham@gmail.com>
2025-09-23 09:49:07 +0530

    CF

---
+++ Codeforces/practice problems/E_Anna_and_the_Valentine_s_Day_Gift.cpp
@@ -0,0 +1,73 @@
+// Choose cpp when the time
+// limit will for sure give tle in pypy
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define INF LLONG_MAX
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define print(arr) for (auto it : arr){cout<<it<<" ";}cout<<endl;
+#define len(arr) arr.size()
+#define printf(x) cout << x << endl;
+#define printm(map) cout<<"{";for(auto it: map){cout<<it.first<<":"<<it.second<<",";};cout<<"}"<<endl;
+
+// const int MOD = 998244353;
+const int MOD = 1e9 + 7;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+// String hashing: sh/shclass, Number: numtheory, SparseTable: SparseTable
+// Segment Tree(lazy propogation): SegmentTree, Merge Sort Tree: sorttree
+// binary indexed tree: BIT, Segment Tree(point updates): SegmentPoint, Convex Hull: hull, Trie/Treap: Tries
+// Combinatorics: pnc, Diophantine Equations: dpheq, Graphs: graphs, DSU: DSU, Geometry: Geometry, FFT: fft
+// Persistent Segment Tree: perseg, FreqGraphs: bgraph
+// Template : https://github.com/thepratholic/CP-Template-Py-Cpp
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    int total_digits = 0;
+    vector<int> numz(n);
+
+    for(int i = 0; i < n; i++) {
+        while(a[i] % 10 == 0) {
+            total_digits ++;
+            numz[i]++;
+            a[i] /= 10;
+        }
+
+
+        while(a[i] > 0) {
+            total_digits++;
+            a[i] /= 10;
+        }
+    }
+
+    sort(numz.begin(), numz.end(), greater<>());
+
+    for(int i = 0; i < n; i += 2) {
+        total_digits -= numz[i];
+    }
+
+    if(total_digits > m) {
+        cout << "Sasha\n";
+    }
+
+    else cout << "Anna\n";
+
+}
+
+int32_t main() {
+    fastio
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
