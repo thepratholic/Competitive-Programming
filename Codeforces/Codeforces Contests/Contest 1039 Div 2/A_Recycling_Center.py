95412510fa9df1e5b9d0af08996d26a3435dc31a
thepratholic <chelaramanipratham@gmail.com>
2025-09-27 18:01:22 +0530

    Upsolved

--- Codeforces/Codeforces Contests/Contest 1039 Div 2/A_Recycling_Center.py
+++ Codeforces/Codeforces Contests/Contest 1039 Div 2/A_Recycling_Center.py
@@ -3,27 +3,12 @@ for _ in range(t):
     n, c = map(int, input().split())
     a = list(map(int, input().split()))
     
-    coins = 0
-    dl = []
-    
-    for x in a:
-        if x > c:
-            coins += 1
-        else:
-            v = c // x
-            d = 0
-            while (1 << (d + 1)) <= v:
-                d += 1
-            dl.append(d)
-    
-    dl.sort()
-    t = 0
-    freeOK = 0
-    
-    for d in dl:
-        if t <= d:
-            freeOK += 1
-            t += 1
-    
-    coins += len(dl) - freeOK
-    print(coins)
+    a.sort(reverse=True)
+
+    s = 0
+
+    for i in range(n):
+        if a[i] * (1 << s) <= c:
+            s += 1
+
+    print(n - s)
