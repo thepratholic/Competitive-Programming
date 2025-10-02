15d6063f2560d81ef89f88223b5c50bcb31c0fbb
thepratholic <chelaramanipratham@gmail.com>
2025-10-02 17:25:37 +0530

    Upsolved

--- LeetCode/Weekly Contest 463/Best Time to Buy and Sell Stock using Strategy.py
+++ LeetCode/Weekly Contest 463/Best Time to Buy and Sell Stock using Strategy.py
@@ -4,27 +4,28 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
         n = len(prices)
-        half = k // 2
 
         original = 0
         for i in range(n):
             original += strategy[i] * prices[i]
 
 
-        prefix_old = [0] * (n + 1) 
-        prefix_price = [0] * (n + 1)   
-        for i in range(n):
-            prefix_old[i + 1] = prefix_old[i] + strategy[i] * prices[i]
-            prefix_price[i + 1] = prefix_price[i] + prices[i]
+        ans = original
 
-        best = original
+        slide = 0
+        for i in range(k):
+            original -= prices[i] * strategy[i]
 
-        for start in range(n - k + 1):
-            end = start + k
-            old_sum = prefix_old[end] - prefix_old[start]
-            new_sum = prefix_price[end] - prefix_price[start + half]
-            cand = original - old_sum + new_sum
-            if cand > best:
-                best = cand
+        for i in range(k // 2, k):
+            slide += prices[i]
 
-        return best
+        ans = max(ans, original + slide)
+
+        for i in range(k, n):
+            slide += prices[i]
+            slide -= prices[i - k//2]
+            original += prices[i - k] * strategy[i - k]
+            original -= prices[i] * strategy[i]
+            ans = max(ans, slide + original)
+
+        return ans
