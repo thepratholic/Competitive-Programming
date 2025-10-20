c13deae2b2692d600a6ce6758c2b79dd01e17a45
thepratholic <chelaramanipratham@gmail.com>
2025-10-20 21:45:06 +0530

    Upsolved

--- Codeforces/Codeforces Contests/Contest 1059 Div 3/C_Beautiful_XOR.py
+++ Codeforces/Codeforces Contests/Contest 1059 Div 3/C_Beautiful_XOR.py
@@ -1,93 +1,50 @@
-import sys,math,cmath,random,os
-from heapq import heappush,heappop
-from bisect import bisect_right,bisect_left
-from collections import Counter,deque,defaultdict
-from itertools import permutations,combinations
-from io import BytesIO, IOBase
-from decimal import Decimal,getcontext
+import sys
+import math
+import bisect
+import heapq
+import itertools
+import collections
+import functools
+import operator
+from collections import defaultdict, Counter, deque
+from heapq import heappush, heappop, heapify
+from math import gcd, ceil, floor, sqrt
 
-BUFSIZE = 8192
-class FastIO(IOBase):
-    newlines = 0
-    def __init__(self, file):
-        self._file = file
-        self._fd = file.fileno()
-        self.buffer = BytesIO()
-        self.writable = "x" in file.mode or "r" not in file.mode
-        self.write = self.buffer.write if self.writable else None
-    def read(self):
-        while True:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            if not b:
-                break
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines = 0
-        return self.buffer.read()
-    def readline(self):
-        while self.newlines == 0:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            self.newlines = b.count(b"\n") + (not b)
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines -= 1
-        return self.buffer.readline()
-    def flush(self):
-        if self.writable:
-            os.write(self._fd, self.buffer.getvalue())
-            self.buffer.truncate(0), self.buffer.seek(0)
-class IOWrapper(IOBase):
-    def __init__(self, file):
-        self.buffer = FastIO(file)
-        self.flush = self.buffer.flush
-        self.writable = self.buffer.writable
-        self.write = lambda s: self.buffer.write(s.encode("ascii"))
-        self.read = lambda: self.buffer.read().decode("ascii")
-        self.readline = lambda: self.buffer.readline().decode("ascii")
-sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
-
-MOD = 10**9 + 7
-RANDOM = random.randrange(1,2**62)
-def gcd(a,b):
-    while b:
-        a,b = b,a%b
-    return a
-def lcm(a,b):
-    return a//gcd(a,b)*b
-def w(x):
-    return x ^ RANDOM
-II = lambda : int(sys.stdin.readline().strip())
-LII = lambda : list(map(int, sys.stdin.readline().split()))
-MI = lambda x : x(map(int, sys.stdin.readline().split()))
-SI = lambda : sys.stdin.readline().strip()
-SLI = lambda : list(map(lambda x:ord(x)-97,sys.stdin.readline().strip()))
-LII_1 = lambda : list(map(lambda x:int(x)-1, sys.stdin.readline().split()))
-LII_C = lambda x : list(map(x, sys.stdin.readline().split()))
-MATI = lambda x : [list(map(int, sys.stdin.readline().split())) for _ in range(x)]
+input = lambda: sys.stdin.readline().strip()
 
 def solve():
-    a, b = LII()
-    
+    a, b = map(int, input().split())
+
     if a == b:
         print(0)
         return
     
-    x = a ^ b
-    if x <= a:
-        print(1)
-        print(x)
-        return
-    
-    m = a | b
-    x1 = a ^ m
-    x2 = m ^ b
-    
-    if x1 <= a and x2 <= m:
-        print(2)
-        print(x1, x2)
-        return
-    
-    print(-1)
+    c = a ^ b
 
-for _ in range(II()):
-    solve()
+    def msb_pos(x):
+        return x.bit_length() - 1
+    
+    msbA = msb_pos(a)
+    msbB = msb_pos(b)
+
+    if msbA < msbB:
+        print(-1)
+        return
+    
+    if a >= c:
+        print(1)
+        print(c)
+        return
+    
+    else:
+        print(2)
+        print(b, a)
+        return
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
