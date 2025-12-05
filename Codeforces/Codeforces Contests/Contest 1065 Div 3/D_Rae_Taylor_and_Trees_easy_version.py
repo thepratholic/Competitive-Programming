b72cb9a46211f2ba6dea1944da2b6a222342d5aa
thepratholic <chelaramanipratham@gmail.com>
2025-12-05 17:06:10 +0530

    Upsolved

---
+++ Codeforces/Codeforces Contests/Contest 1065 Div 3/D_Rae_Taylor_and_Trees_easy_version.py
@@ -0,0 +1,36 @@
+# Three Golden Rules :
+# 1) Solution is Simple
+# 2) Proof is Simple
+# 3) Implementation is Simple
+
+
+import sys
+from collections import defaultdict, Counter, deque
+from heapq import heappush, heappop, heapify
+from math import gcd, ceil, floor, sqrt
+from functools import lru_cache, reduce
+from bisect import bisect, bisect_left, bisect_right
+from itertools import accumulate, permutations, groupby
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    mp = Counter(a)
+
+    mini = n + 1
+
+    for i in range(n - 1):
+        mini = min(mini, a[i])
+
+        if (n - i) == mini:
+            print("No")
+            return
+        
+    print("Yes")
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        solve()
