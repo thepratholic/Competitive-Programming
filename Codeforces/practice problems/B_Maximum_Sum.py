31e540b0497cbaa81014782796bca2b756d98b90
thepratholic <chelaramanipratham@gmail.com>
2026-01-13 10:51:27 +0530

    CF

--- Codeforces/practice problems/B_Maximum_Sum.py
+++ Codeforces/practice problems/B_Maximum_Sum.py
@@ -1,69 +1,102 @@
-import sys, io, os
-import math
-import bisect
-import heapq
-import string
-import re
-from decimal import *
-from random import randrange, choice, shuffle
-from types import GeneratorType
-from collections import defaultdict, Counter, deque
+import sys,math,cmath,random,os
+from heapq import heappush,heappop
+from bisect import bisect_right,bisect_left
+from collections import Counter,deque,defaultdict
+from itertools import permutations,combinations
+from io import BytesIO, IOBase
+from decimal import Decimal,getcontext
 
-input = sys.stdin.readline
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
 
-def I(): return input()
-def II(): return int(input())
-def MII(): return map(int, input().split())
-def LI(): return list(input().split())
-def LII(): return list(map(int, input().split()))
-def GMI(): return map(lambda x: int(x) - 1, input().split())
-def LGMI(): return list(map(lambda x: int(x) - 1, input().split()))
-def WRITE(out): return print('\n'.join(map(str, out)))
-def WS(out): return print(' '.join(map(str, out)))
-def WNS(out): return print(''.join(map(str, out)))
-def WSNOPRINT(out): return ''.join(map(str, out))
-
-def bootstrap(f, stack=[]):
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
-                        break
-                    to = stack[-1].send(to)
-            return to
-    return wrappedfunc
-
+# functions #
+# MOD = 998244353
 MOD = 10**9 + 7
-#@bootstrap
+RANDOM = random.randrange(1,2**62)
+def gcd(a,b):
+    while b:
+        a,b = b,a%b
+    return a
+def lcm(a,b):
+    return a//gcd(a,b)*b
+def w(x):
+    return x ^ RANDOM
+II = lambda : int(sys.stdin.readline().strip())
+LII = lambda : list(map(int, sys.stdin.readline().split()))
+MI = lambda x : x(map(int, sys.stdin.readline().split()))
+SI = lambda : sys.stdin.readline().strip()
+SLI = lambda : list(map(lambda x:ord(x)-97,sys.stdin.readline().strip()))
+LII_1 = lambda : list(map(lambda x:int(x)-1, sys.stdin.readline().split()))
+LII_C = lambda x : list(map(x, sys.stdin.readline().split()))
+MATI = lambda x : [list(map(int, sys.stdin.readline().split())) for _ in range(x)]
+##
+
+#String hashing: shclass, fenwick sortedlist: fsortl, Number: numtheory/numrare, SparseTable: SparseTable
+#Bucket Sorted list: bsortl, Segment Tree(lp,selfop): SegmentTree, bootstrap: bootstrap, Trie: tries
+#binary indexed tree: BIT, Segment Tree(point updates): SegmentPoint, Convex Hull: hull, BitArray: bitarray
+#Combinatorics: pnc, Diophantine Equations: dpheq, DSU: DSU, Geometry: Geometry, FFT: fft, XOR_dict: xdict
+#Persistent Segment Tree: perseg, Binary Trie: b_trie, HLD: hld, String funcs: sf, Segment Tree(lp): SegmentOther
+#Graph1(dnc,bl): graphadv, Graph2(khn,sat): 2sat, Graph3(fltn,bprt): graphflatten, Graph4(ep,tp,fw,bmf): graphoth
+#Graph5(djik,bfs,dfs): graph, Graph6(dfsin): dfsin, utils: utils, Persistent DSU: perdsu, Merge Sort Tree: sorttree
+#2-D BIT: 2DBIT, MonoDeque: mono, nummat: matrix, SuffixAutomaton: sautomaton, linalg: linalg, SquareRtDecomp: sqrt
+#Grapth7(bridges): graph_dmgt, FWHT(^,|,&): fwht
+#Template : https://github.com/OmAmar106/Template-for-Competetive-Programming
+#if os.environ.get('LOCAL'):sys.stdin = open(r'input.txt', 'r');sys.stdout = open(r'output.txt','w')
+
 def solve():
-    # Your solution here
-    n, k = MII()
+    L = LII()
+    n, k = L[0], L[1]
     a = LII()
 
-    ans, cur = 0, 0
+    a.sort()
+    pref = [0] * (n + 1)
 
-    for i in a:
-        cur += i
+    for i in range(n):
+        pref[i + 1] = pref[i] + a[i]
+
+    ans = 0
+    for i in range(k + 1):
+        cur = pref[n - (k - i)] - pref[2 * i]
         ans = max(ans, cur)
-        cur = max(cur, 0)
 
-    final = sum(a) % MOD
+    print(ans)
 
-    for i in range(k):
-        final = (final + ans) % MOD
-        ans = (ans * 2) % MOD
-
-    print(final)
-
-def main():
-    for _ in range(II()):
-        solve()
-main()
+for _ in range(II()):
+    solve()
