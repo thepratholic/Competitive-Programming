38716505666cd39210d37236182888ddd8c02135
thepratholic <chelaramanipratham@gmail.com>
2026-03-30 14:27:47 +0530

    Upsolved

--- LeetCode/Weekly Contest 495/Incremental Even-Weighted Cycle Queries.py
+++ LeetCode/Weekly Contest 495/Incremental Even-Weighted Cycle Queries.py
@@ -1,46 +1,60 @@
-from typing import List
-
-
 class DSU:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.size = [1] * (n)
-        self.parity = [0] * (n)
+        self.size = [1] * n
+        self.parity = [0] * n
 
-    def findUPar(self, node): # do the path compression
+    def find(self, node):
         if node == self.parent[node]:
             return node
 
-        par = self.parent[node]
-        self.parent[node] = self.findUPar(self.parent[node])
-        self.parity[node] ^= self.parity[par]
+        original_par = self.parent[node]
+        root = self.find(original_par)
+
+        self.parity[node] ^= self.parity[original_par]
+        self.parent[node] = root
+
         return self.parent[node]
 
     def union(self, u, v, wt):
-        ulp_u = self.findUPar(u)
-        ulp_v = self.findUPar(v)
+        root_u = self.find(u)
+        root_v = self.find(v)
 
-        if ulp_u == ulp_v: return (self.parity[u] ^ self.parity[v] ^ wt) == 0
-            
-        elif self.size[ulp_u] < self.size[ulp_v]:
-            ulp_u, ulp_v = ulp_v, ulp_u
-            u, v = v, u
+        if root_u == root_v:
+            return
 
-        self.parent[ulp_v] = ulp_u
-        self.parity[ulp_v] = self.parity[v] ^ self.parity[u] ^ wt
-        self.size[ulp_u] += self.size[ulp_v]
+        if self.size[root_u] < self.size[root_v]:
+            self.size[root_v] += self.size[root_u]
+            self.parent[root_u] = root_v
+            self.parity[root_u] = self.parity[u] ^ wt ^ self.parity[v]
 
-        return True
+        else:
+            self.size[root_u] += self.size[root_v]
+            self.parent[root_v] = root_u
+            self.parity[root_v] = self.parity[v] ^ wt ^ self.parity[u]
 
-    # my own DSU class, copied from github
+    
+    def get_parity(self, node):
+        return self.parity[node]
 
 class Solution:
     def numberOfEdgesAdded(self, n: int, edges: List[List[int]]) -> int:
-
         dsu = DSU(n)
+
         ans = 0
         for u, v, wt in edges:
-            if dsu.union(u, v, wt):
+            root_u = dsu.find(u)
+            root_v = dsu.find(v)
+
+            if root_u == root_v:
+                path_sum_from_u = dsu.get_parity(u)
+                path_sum_from_v = dsu.get_parity(v)
+
+                if path_sum_from_u ^ path_sum_from_v ^ wt == 0:
+                    ans += 1
+
+            else:
                 ans += 1
+                dsu.union(u, v, wt)
 
         return ans
