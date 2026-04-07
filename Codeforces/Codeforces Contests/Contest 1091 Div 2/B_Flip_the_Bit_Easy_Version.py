89d6e26113b4a14bc6a456cd71b121411b74e7c1
thepratholic <chelaramanipratham@gmail.com>
2026-04-07 21:09:03 +0530

    CF Round 1091 Div 2

---
+++ Codeforces/Codeforces Contests/Contest 1091 Div 2/B_Flip_the_Bit_Easy_Version.py
@@ -0,0 +1,76 @@
+import sys,math,cmath,random,os
+from heapq import heappush,heappop
+from bisect import bisect_right,bisect_left
+from collections import Counter,deque,defaultdict
+from itertools import permutations,combinations
+from io import BytesIO, IOBase
+from decimal import Decimal,getcontext
+
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+II = lambda : int(sys.stdin.readline().strip())
+LII = lambda : list(map(int, sys.stdin.readline().split()))
+
+def cnt(b):
+    b = b + [0]
+    return sum(b[i] != b[i-1] for i in range(1, len(b)))
+
+def solve():
+    n, k = LII()
+    a = LII()
+    p = LII()[0] - 1 
+    x = a[p]
+    
+    arr = [1 if a[i] != x else 0 for i in range(n)]
+    
+    right_b = arr[p+1:]      
+    left_b = arr[p-1::-1]     
+    
+    r = cnt(right_b)
+    l = cnt(left_b)
+    
+    ans = max(r, l)
+    if ans % 2 == 1:
+        ans += 1
+    print(ans)
+
+for _ in range(II()):
+    solve()
