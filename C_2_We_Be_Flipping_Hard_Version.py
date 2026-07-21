543341fe7a6d9ef5b581e24a24fa46b385aa0a78
thepratholic <chelaramanipratham@gmail.com>
2026-07-21 18:57:34 +0530

    Changed the code

--- C_2_We_Be_Flipping_Hard_Version.py
+++ C_2_We_Be_Flipping_Hard_Version.py
@@ -12,29 +12,14 @@ from decimal import *
 from fractions import Fraction
 import re
 
-input = sys.stdin.readline
-
+input = stdin.readline
 
 def solve():
+    # Write your solution here
     n = int(input())
     a = list(map(int, input().split()))
 
-    flip = 0
-    ans = []
-
-    for i in range(n - 1, -1, -1):
-
-        cur = a[i]
-        if flip:
-            cur = -cur
-
-        if cur > 0:
-            ans.append(i + 1)
-            flip ^= 1
-
-    print(len(ans))
-    print(*ans)
-
+    
 
 t = int(input())
 for _ in range(t):
